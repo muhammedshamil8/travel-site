ecc027319f0ab163b42760b3a796c1ca66127454
Shamil <muhammedshamil008@gmail.com>
2023-09-20 02:04:13 +0530

    improved more to responsive and some other styles

---
+++ scripts/data/review.js
@@ -0,0 +1,57 @@
+const review = [
+     {
+          Image:'images/cat1.jpeg',
+          experience:`But i must explain to you how all this mistaken idea of denouncing pleasure and praising
+          pain was born and i will give you a complete account of the system and expound the
+          actual teachings of the great explorer of the truth, the master-builder of human
+          happines.`,
+          rating:'5',
+          name:' John Doe',
+          about:'Accountant'
+
+},
+     {
+          Image:'images/cat2.jpeg',
+          experience:`But i must explain to you how all this mistaken idea of denouncing pleasure and praising
+          pain was born and i will give you a complete account of the system and expound the
+          actual teachings of the great explorer of the truth, the master-builder of human
+          happines.`,
+          rating:'5',
+          name:' John Smith',
+          about:'Journalist, HWO News'
+
+},
+     {
+          Image:'images/login.jpeg',
+          experience:`But i must explain to you how all this mistaken idea of denouncing pleasure and praising
+          pain was born and i will give you a complete account of the system and expound the
+          actual teachings of the great explorer of the truth, the master-builder of human
+          happines.`,
+          rating:'5',
+          name:'Tanara Bellis',
+          about:'Managing Director,JTH'
+
+},
+     {
+          Image:'images/login.jpeg',
+          experience:`But i must explain to you how all this mistaken idea of denouncing pleasure and praising
+          pain was born and i will give you a complete account of the system and expound the
+          actual teachings of the great explorer of the truth, the master-builder of human
+          happines.`,
+          rating:'5',
+          name:'Kunnapu',
+          about:'Student'
+
+},
+     {
+          Image:'images/cat3.jpeg',
+          experience:`But i must explain to you how all this mistaken idea of denouncing pleasure and praising
+          pain was born and i will give you a complete account of the system and expound the
+          actual teachings of the great explorer of the truth, the master-builder of human
+          happines.`,
+          rating:'5',
+          name:'Ronaldo',
+          about:'Football player'
+
+},
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 let packagesHtml = '';
 
-packages.forEach((package)=>{
-     packagesHtml +=`
+packages.forEach((package) => {
+    packagesHtml += `
      <li class="package-card">
      <div class="package-image">
          <img src="${package.image}" alt="package-img" draggable="false">
@@ -20,5 +20,37 @@ packages.forEach((package)=>{
      `
 });
 document.querySelector('.js-package-grid')
-.innerHTML = packagesHtml;
+    .innerHTML = packagesHtml;
+
+
+let reviewHtml = '';
+
+review.forEach((review) => {
+    reviewHtml += `
+     <li class="experience-card">
+<div class="experience-card-logo">
+    <img src="${review.Image}" alt="profile-pic">
+</div>
+<div class="experience-card-details">
+    <p class="review-p-1">
+        ${review.experience}
+    </p>
+    <P class="review-p-2">
+        <img src="/images/rating-${review.rating * 10}.png" alt="ratings">
+    </P>
+    <p class="review-p-3">
+        ${review.name}<br><span>${review.about}</span>
+    </p>
+
+</div>
+</li>
+     `
+});
+document.querySelector('.js-experience-grid')
+    .innerHTML = reviewHtml;
+
+
+
+
+
 

dc56c7330e1eb5eab71d70a8aef9144954648267
Shamil <muhammedshamil008@gmail.com>
2023-09-20 02:23:07 +0530

    some images also github pages dont getting solving

--- scripts/data/package.js
+++ scripts/data/package.js
@@ -1,35 +1,35 @@
 const packages = [
      {
           id: "id1-p",
-          image: "/images/alleppey.png",
+          image: "alleppey.png",
           place: "Alleppey",
           about: `There is the whole of kerala in one side,<br>and then there is this heavenly tourist <br>destionation called Alappuzha or Alleppey!`,
           priceRupees: 2500
      },
      {
           id: "id2-p",
-          image: "/images/wayanad.png",
+          image: "wayanad.png",
           place: "Wayanad",
           about: `Cradled in the lap of the Western Ghats,<br>Wayanad truly deserves a top rank in the <br>list of places to visit in kerala.`,
           priceRupees: 2000
      },
      {
           id: "id3-p",
-          image: "/images/kochi.png",
+          image: "kochi.png",
           place: "Kochi",
           about: `<u>Kochi</u> or cochin is a unique placein <u>God's <br>Own Country</u> that is perfectly blended with<br> cultural vlaues and modern ideas!`,
           priceRupees: 3000
      },
      {
           id: "id4-p",
-          image: "/images/kovalam.png",
+          image: "kovalam.png",
           place: "Kovalam",
           about: `Cradled in the lap of the Western Ghats,<br>Wayanad truly deserves a top rank in the <br>list of places to visit in kerala.`,
           priceRupees: 1800
      },
      {
           id: "id5-p",
-          image: "/images/munnar.png",
+          image: "munnar.png",
           place: "Munnar",
           about: `There is the whole of kerala in one side,<br>and then there is this heavenly tourist <br>destionation called Alappuzha or Alleppey!`,
           priceRupees: 2800
@@ -37,7 +37,7 @@ const packages = [
      ,
      {
           id: "id6-p",
-          image: "/images/thekkady.png",
+          image: "thekkady.png",
           place: "Thekkady",
           about: `There is the whole of kerala in one side,<br>and then there is this heavenly tourist <br>destionation called Alappuzha or Alleppey!`,
           priceRupees: 2200
@@ -45,7 +45,7 @@ const packages = [
      ,
      {
           id: "id7-p",
-          image: "/images/travel5.jpeg",
+          image: "travel5.jpeg",
           place: "Allapuzha",
           about: `There is the whole of kerala in one side,<br>and then there is this heavenly tourist <br>destionation called Alappuzha or Alleppey!`,
           priceRupees: 2900
